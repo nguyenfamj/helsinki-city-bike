12352e38bf9c80e5a2e063c1436f61ee6cca0fc7
nguyenfamj <nguyenfamj1409@gmail.com>
2022-07-16 23:16:56 +0700

    Added endpoint to get single station data with relevant information

--- server/src/api/controllers/station.ts
+++ server/src/api/controllers/station.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import db from '../../db/index';
 
+// Import interfaces
+import { stationData } from '../interfaces/station';
 // Import helper functions
 import { fullTextSearchProcessor } from '../helpers/textProcessing';
 
@@ -25,7 +27,10 @@ export const getStations = async (req: Request, res: Response): Promise<void> =>
 
     // Query initiation
     const { rows: countRow } = await db.query(countQueryString, countParams);
-    const { rows: stationRows } = await db.query(stationsQueryString, queryParams);
+    const { rows: stationRows }: { rows: stationData[] } = await db.query(
+      stationsQueryString,
+      queryParams
+    );
 
     // Success response
     res.status(200).json({
@@ -36,7 +41,6 @@ export const getStations = async (req: Request, res: Response): Promise<void> =>
       size,
       stations: stationRows,
     });
-    //
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: 'Internal Server Error' });
@@ -44,5 +48,90 @@ export const getStations = async (req: Request, res: Response): Promise<void> =>
 };
 
 export const getSingleStation = async (req: Request, res: Response): Promise<void> => {
-  const { id: station_id } = req.params;
+  try {
+    const { id: station_id } = req.params;
+    if (!station_id) throw new Error('Station id not found on query params');
+
+    // Query Params
+    const stationDataParams: any[] = [station_id];
+    const tempTableParams: any[] = [station_id];
+
+    // Prepared query string
+    // Station data
+    const stationDataQueryString = `SELECT station_id, fi_name, se_name, en_name, fi_address, se_address, fi_city, se_city, operator_name, capacity, longitude, latitude
+    FROM station_data
+    WHERE station_id=$1`;
+    // Create temporary table in the database for querying in this session
+    const stationTempTableQueryString: string = `CREATE TEMP TABLE station_${station_id} AS
+    (SELECT targeted_station.station_id, targeted_station.fi_name, targeted_station.se_name, targeted_station.en_name,
+           targeted_station.fi_address, targeted_station.se_address, targeted_station.fi_city, targeted_station.se_city,
+           targeted_station.operator_name, targeted_station.capacity, targeted_station.longitude, targeted_station.latitude,
+           journey_data.covered_distance, journey_data.duration, journey_data.departure_station_id, journey_data.departure_station_name,
+           journey_data.return_station_id,journey_data.return_station_name
+    FROM
+    (SELECT station_id, fi_name, se_name, en_name, fi_address, se_address, fi_city, se_city, operator_name, capacity, longitude, latitude
+    FROM station_data
+    WHERE station_id=$1) AS targeted_station
+    INNER JOIN journey_data
+        ON targeted_station.station_id=journey_data.departure_station_id
+        OR targeted_station.station_id=journey_data.return_station_id)`;
+    // Journey starting from the station
+    const journeysFromStationQueryString = `SELECT COUNT(*) FROM station_${station_id} WHERE station_id=departure_station_id;`;
+    // Journeys ending at the station
+    const journeysToStationQueryString = `SELECT COUNT(*) FROM station_${station_id} WHERE station_id=return_station_id;`;
+    // Average distance from the station
+    const avgDistanceFromStationQueryString = `SELECT avg(covered_distance) FROM station_${station_id} WHERE station_id=departure_station_id;`;
+    // Average distance to the station
+    const avgDistanceToStationQueryString = `SELECT avg(covered_distance) FROM station_${station_id} WHERE station_id=return_station_id;`;
+    // Top 5 destination from the station
+    const topPlacesFromStationQueryString = `SELECT return_station_name, return_station_id, COUNT(return_station_name) AS counted
+    FROM station_${station_id}
+    WHERE station_id !=station_${station_id}.return_station_id
+    AND return_station_name IS NOT NULL
+    GROUP BY return_station_name, return_station_id
+    ORDER BY counted DESC
+    LIMIT 5;`;
+    // Top 5 departures stations for journeys ending at the station
+    const topPlacesToStationQueryString = `SELECT departure_station_name, departure_station_id, COUNT(departure_station_name) AS counted
+    FROM station_${station_id}
+    WHERE station_id !=station_${station_id}.departure_station_id
+    AND return_station_name IS NOT NULL
+    GROUP BY departure_station_name, departure_station_id
+    ORDER BY counted DESC
+    LIMIT 5;`;
+    // Drop temp table after session is finished
+    const dropTempTable = `DROP TABLE IF EXISTS station_${station_id}`;
+
+    // Query initiation
+    const { rows: stationData }: { rows: stationData[] } = await db.query(
+      stationDataQueryString,
+      stationDataParams
+    );
+    await db.query(stationTempTableQueryString, tempTableParams);
+    const { rows: journeysFromStation } = await db.query(journeysFromStationQueryString);
+    const { rows: journeysToStation } = await db.query(journeysToStationQueryString);
+    const { rows: avgDistanceFromStation } = await db.query(avgDistanceFromStationQueryString);
+    const { rows: avgDistanceToStation } = await db.query(avgDistanceToStationQueryString);
+    const { rows: topPlacesFromStation } = await db.query(topPlacesFromStationQueryString);
+    const { rows: topPlacesToStation } = await db.query(topPlacesToStationQueryString);
+    await db.query(dropTempTable);
+
+    // Success response
+    res.status(200).json({
+      success: true,
+      message: `Station ${stationData[0].fi_name} data retrieved successfully`,
+      stationData,
+      journeysFromStation: journeysFromStation[0].count,
+      journeysToStation: journeysToStation[0].count,
+      avgDistanceFromStation: avgDistanceFromStation[0].avg,
+      avgDistanceToStation: avgDistanceToStation[0].avg,
+      topPlacesFromStation,
+      topPlacesToStation,
+    });
+
+    console.log(`LOG: station's ${station_id} accessed`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
 };
---
+++ server/src/api/interfaces/station.ts
@@ -0,0 +1,14 @@
+export interface stationData {
+  station_id: number;
+  fi_name: string;
+  se_name: string;
+  en_name: string;
+  fi_address: string;
+  se_address: string;
+  fi_city: string;
+  se_city: string;
+  operator_name: string;
+  capacity: number;
+  longitude: string;
+  latitude: string;
+}
